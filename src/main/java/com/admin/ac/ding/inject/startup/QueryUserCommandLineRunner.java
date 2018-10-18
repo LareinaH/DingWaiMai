d48d0b770df886c0aca69804d4c0ee740e19e43a
lquterqtd <378203086@qq.com>
2018-10-18 22:48:59 +0800

    add

--- src/main/java/com/admin/ac/ding/inject/startup/QueryUserCommandLineRunner.java
+++ src/main/java/com/admin/ac/ding/inject/startup/QueryUserCommandLineRunner.java
@@ -1,13 +1,18 @@
 package com.admin.ac.ding.inject.startup;
 
 import com.admin.ac.ding.mapper.OrderMapper;
+import com.admin.ac.ding.mapper.SysRoleMapper;
 import com.admin.ac.ding.model.Order;
+import com.admin.ac.ding.model.SysRole;
 import com.admin.ac.ding.service.CacheService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @Component
 public class QueryUserCommandLineRunner implements CommandLineRunner {
@@ -19,6 +24,9 @@ public class QueryUserCommandLineRunner implements CommandLineRunner {
     @Autowired
     OrderMapper orderMapper;
 
+    @Autowired
+    SysRoleMapper sysRoleMapper;
+
     @Override
     public void run(String... strings) throws Exception {
         orderMapper.select(new Order()).stream().forEach(x -> {
@@ -28,6 +36,15 @@ public class QueryUserCommandLineRunner implements CommandLineRunner {
 
             }
         });
+
+        List<SysRole> sysRoleList = sysRoleMapper.select(new SysRole());
+        for (SysRole sysRole : sysRoleList) {
+            try {
+                cacheService.getUserDetail(sysRole.getUserId());
+            } catch (Exception e) {
+
+            }
+        }
 
         logger.info("cache stat:{}", cacheService.getCacheStat());
         logger.info("cache keys:{}", cacheService.getCacheKeys());
