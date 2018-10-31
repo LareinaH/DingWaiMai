9febdb9b1e84e3888b6ead6f99d1833f557f3d98
Lareina_H <Lareina_h@163.com>
2018-10-31 13:58:40 +0800

    add DeptListLocalMapper

---
+++ src/main/java/com/admin/ac/ding/mapper/DeptListLocalMapper.java
@@ -0,0 +1,7 @@
+package com.admin.ac.ding.mapper;
+
+import com.admin.ac.ding.base.BaseMapper;
+import com.admin.ac.ding.model.DeptListLocal;
+
+public interface DeptListLocalMapper extends BaseMapper<DeptListLocal> {
+}
